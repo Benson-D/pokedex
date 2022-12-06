e06b25da34087f41102e8cbd5b01a7bdba93d71b
Dan Benson <dbenson3091@gmail.com>
2022-12-05 22:41:30 -0600

    updated battle page to display pokemon for game

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,8 +1,18 @@
 import { PokeStats } from '../interface/pokeInterface';
 
-function Player({ pokemon }: { pokemon: PokeStats }) {
+function Player({ pokemon }: { pokemon: PokeStats[] }) {
   return (
-    <div>Pokemon</div>
+    <section className="max-w-sm rounded shadow shadow-blue-800 mx-10 p-10">
+      <h3 className="font-bold text-xl text-center mb-2">
+        {pokemon[0]?.name.toUpperCase()}
+      </h3>
+      <div>
+        <img className="w-40 h-40" src={pokemon[0]?.image}></img>
+      </div>
+      <p className="text-gray-700 text-base text-center">
+        HP: {pokemon[0].experience}
+      </p>
+    </section>
   )
 };
 
--- src/pages/PokeBattle.tsx
+++ src/pages/PokeBattle.tsx
@@ -1,22 +1,36 @@
 import { useState } from 'react';
+import Player from '../components/Player';
 import { PokeStats } from '../interface/pokeInterface';
 import { selectPlayers } from '../utilities/helper';
 
 function PokeBattle({ pokemon }: { pokemon: PokeStats[]}) {
-  const [playerOne, setPlayerOne] = useState();
-  const [playerTwo, setPlayerTwo] = useState();
+  const [playerOne, setPlayerOne] = useState<PokeStats[]>([]);
+  const [playerTwo, setPlayerTwo] = useState<PokeStats[]>([]);
+  const [loadPlayer, setLoadPlayer] = useState(false);
 
   const loadPlayers = () => {
-    const players = selectPlayers(pokemon);
-    console.log(players);
+    const [setOne, setTwo] = selectPlayers(pokemon);
+    setPlayerOne(setOne);
+    setPlayerTwo(setTwo)
+    setLoadPlayer(true);
   }
 
   return (
-    <section className="flex flex-col justify-center">
-        <h1 className="text-center">Let's test your skills to be a pokemon master</h1>
-        <div>
-            <button onClick={loadPlayers}>Start</button>
+    <section>
+        <h1 className="text-center font-medium text-base leading-4">
+          Let's test your skills to be a pokemon master
+        </h1>
+        <div className="mt-10 flex justify-center">
+            <button className="bg-red-200 py-2 px-6 hover:bg-red-400 rounded" onClick={loadPlayers}>
+              Start
+            </button>
         </div>
+        {loadPlayer && 
+          <div className="mt-20 flex justify-center">
+            <Player pokemon={playerOne} /> 
+            <Player pokemon={playerTwo} /> 
+          </div>
+        }
     </section>
   );
 };
