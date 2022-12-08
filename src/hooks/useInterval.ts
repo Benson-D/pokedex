84ff2f742ba8088d971b5f2b1c3c831518cdb796
Dan Benson <dbenson3091@gmail.com>
2022-12-07 21:09:15 -0600

    created a custom hook that can handle various pokemon when battling

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from 'react';
+
+/**
+ * Utilizes setInterval with custom hooks
+ * @param callback 
+ * @param delay 
+ */
+function useInterval(callback: () => void, delay: number | null): void {
+    const savedCallback = useRef(callback);
+
+    //Remembers previous callback
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    useEffect(() => {
+        if (delay === null && delay !== 0) {
+            return; 
+        }
+
+        const id = setInterval(() => savedCallback.current(), delay);
+        return () => clearInterval(id);
+    }, [delay])
+
+};
+
+export default useInterval;
