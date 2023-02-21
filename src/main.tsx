f30855f837eff61f4eaa75a98591f48a19eaf585
Dan Benson <dbenson3091@gmail.com>
2023-02-20 22:35:07 -0600

    updated files to handle graphql

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider, 
+  from, 
+  HttpLink 
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 import App from './App';
 import './index.css';
 
-// Create client 
-const queryClient = new QueryClient();
+const errorLink = onError(({ networkError }) => {
+  if (networkError) {
+    console.error('GraphQL error:', networkError?.message);
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "https://beta.pokeapi.co/graphql/v1beta" })
+]);
+
+//Create Apollo client
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
