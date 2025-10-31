37f1dc0b6bb244372c99e2b7504c121c86946541
Dan Benson <danmarcbenson@gmail.com>
2025-10-30 21:32:24 -0500

    modified folder structure for easier navigation

---
+++ src/features/table/TableHeader.tsx
@@ -0,0 +1,51 @@
+import { useContext } from "react";
+import { flexRender, HeaderGroup } from "@tanstack/react-table";
+import { BiSortUp, BiSortDown } from "react-icons/bi";
+import { FormattedPokemon } from "../../interface/pokeInterface";
+import ThemeContext from "../../context/ThemeContext";
+
+/** Table Header for Poke Table,
+ * displays column categories,
+ * if table can be sorted will display icon direction of sort
+ *
+ * Props:
+ *    headerGroups: {tanstack table}
+ * State: none
+ */
+function TableHeader({
+  headerGroups,
+}: {
+  headerGroups: () => HeaderGroup<FormattedPokemon>[];
+}) {
+  const { dark } = useContext(ThemeContext);
+  return (
+    <thead className="poke-table-header">
+      {headerGroups().map((headerGroup) => (
+        <tr key={headerGroup.id}>
+          {headerGroup.headers.map((header) => (
+            <th key={header.id} className="p-2">
+              {header.isPlaceholder ? null : (
+                <div
+                  onClick={header.column.getToggleSortingHandler()}
+                  className={`flex justify-between text-xs 
+                                   ${dark ? "text-white" : "text-black"}`}
+                >
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext(),
+                  )}
+                  {{
+                    asc: <BiSortUp />,
+                    desc: <BiSortDown />,
+                  }[header.column.getIsSorted() as string] ?? null}
+                </div>
+              )}
+            </th>
+          ))}
+        </tr>
+      ))}
+    </thead>
+  );
+}
+
+export default TableHeader;
