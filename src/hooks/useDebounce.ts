5f91e263b391cc0fb3086a3cc534f7bd875a2bfd
Dan Benson <danmarcbenson@gmail.com>
2025-10-30 21:35:24 -0500

    modified docs and updated package container

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
 
 /**
- * Custom Hook allows you to debounce fast changing values, e.c. search bar
+ *
+ * A custom React hook that delays updating a value until after a specified delay.
+ * Useful for preventing frequent updates from rapidly changing inputs (e.g. search fields, window resizing, etc.).
  */
 function useDebounce<T>(value: T, delay?: number): T {
   const [debounce, setDebounced] = useState<T>(value);
 
   useEffect(() => {
     const debounceId = setTimeout(() => setDebounced(value), delay || 800);
+
     return () => clearTimeout(debounceId);
   }, [value, delay]);
 
